c6640b740bc74b0c84b046c058f755010334cb68
evgen <evgen1973108@gmail.com>
2024-04-27 16:29:34 +0300

    add homework_20.2

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from catalog.models import Product
 
 
 # Create your views here.
@@ -16,3 +17,15 @@ def contacts(request):
             file.write(f"Ваше сообщение: {name}, {phone}, {message}")
 
     return render(request, "main/contacts.html")
+
+
+def product(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'product_list.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'product_detail.html', context)
