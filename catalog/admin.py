9315dc91722d6978fa97bcd44b4ee3fb6906df37
evgen <evgen1973108@gmail.com>
2024-04-27 16:26:29 +0300

    add homework_20.2

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,7 +6,7 @@ from catalog.models import Product, Category
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "product_name", "price", "category")
+    list_display = ("id", "product_name", "price", "category", "image")
     list_filter = ("category",)
     search_fields = ("product_name", "product_description")
 
