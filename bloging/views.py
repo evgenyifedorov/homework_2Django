ae9f42225bb2b25c788e1ed29cca847811fb9e13
evgen <evgen1973108@gmail.com>
2024-05-06 20:49:40 +0300

    add homework_21ю1

--- bloging/views.py
+++ bloging/views.py
@@ -9,6 +9,11 @@ from bloging.models import Blog
 class BlogListView(ListView):
     model = Blog
 
+    def get_queryset(self, *args, **kwargs):
+        quereyset = super().get_queryset().order_by(*args, **kwargs)
+        quereyset = quereyset.filter(is_published=True)
+        return quereyset
+
 
 class BlogDetailView(DetailView):
     model = Blog
